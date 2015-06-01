163e3b30e077aee09fc6dfbe0d8d0260ec94395b
Adilson dos Santos Dantas <adilson@digirati.com.br>
2015-06-01 17:48:40 -0300

    Fixes

--- dummy-foo.c
+++ dummy-foo.c
@@ -1,3 +1,18 @@
+/*
+
+This package is in public domain, and its licensed by myself.
+
+Anyone can copy and do anything with this package. 
+
+You can do anything, even modify, recompile, relicense to a GPL, Apache, 
+open source, proprietary or any other license. But the original package and source 
+code is still belongs to me (Adilson dos Santos Dantas) and will be in public
+domain forever.
+
+Be warned because this program comes WITHOUT ANY WARRANTY. So don't complain
+if something goes wrong or Murphy does his job again.
+
+*/
 #include<stdio.h>
 
 int main(void)
@@ -21,6 +36,6 @@ printf("      	YOU DON'T SAY?   \n"
 "██████░░░░░░░░░░░░░░░░░░░░▀▄████████████\n"
 "██████░░▄░░░░░░░░░░░░░▄░░░██████████████\n"
 "██████░░░░░░░░░░░░░▄█▀░░▄███████████████\n"
-"███████▄▄░░░░░░░░░▀░░░▄▀▄███████████████");
+"███████▄▄░░░░░░░░░▀░░░▄▀▄███████████████\n\n");
 return 0;
 }
