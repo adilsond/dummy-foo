fedd9b7929888388d62145d5f350621fe8c8fa60
Adilson dos Santos Dantas <adilson@digirati.com.br>
2015-11-11 16:54:12 -0200

    November foo

--- dummy-foo.c
+++ dummy-foo.c
@@ -18,21 +18,7 @@ if something goes wrong or Murphy does his job again.
 int main(void)
 {
 printf("\n"
-"                                      .,'\n"
-"                                   .'`.'\n"
-"                                  .' .'\n"
-"                      _.ood0Pp._ ,'  `.~ .q?00doo._\n"
-"                  .od00Pd0000Pdb._. . _:db?000b?000bo.\n"
-"                .?000Pd0000PP?000PdbMb?000P??000b?0000b.\n"
-"              .d0000Pd0000P'  `?0Pd000b?0'  `?000b?0000b.\n"
-"             .d0000Pd0000?'     `?d000b?'     `?00b?0000b.\n"
-"             d00000Pd0000Pd0000Pd00000b?00000b?0000b?0000b\n"
-"             ?00000b?0000b?0000b?b    dd00000Pd0000Pd0000P\n"
-"             `?0000b?0000b?0000b?0b  dPd00000Pd0000Pd000P'\n"
-"              `?0000b?0000b?0000b?0bd0Pd0000Pd0000Pd000P'\n"
-"                `?000b?00bo.   `?P'  `?P'   .od0Pd000P'\n"
-"                  `~?00b?000bo._  .db.  _.od000Pd0P~'\n"
-"                      `~?0b?0b?000b?0Pd0Pd000PdP~'\n"
+"November foo"
 "\n\n");
 return 0;
 }
